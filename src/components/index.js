f86d346693b00d6b1ca448998f02fe8a2b9b2922
David Capella <D_Capella@yahoo.com>
2018-09-17 07:41:39 -0500

    added test not for heroku

--- src/components/index.js
+++ src/components/index.js
@@ -7,15 +7,15 @@ export class DisplayQuote extends React.Component {
       color: "white",
     };
     return (
-      <div className="jumbotron">
+      <div className="jumbotron" id="quote-box">
         <h2 className="text-center">Quotes</h2>
         <div className="breakLineTop"></div>
-        <p className="text-center">"{this.props.quote}"</p>
-        <p className="text-right authorName">- {this.props.author}</p>
+        <p className="text-center" id="text">"{this.props.quote}"</p>
+        <p className="text-right authorName" id="author">- {this.props.author}</p>
         <div className="breakLineBottom"></div>
         <div className="bottomButtons">
-          <a onClick={this.props.tweetQuote} href={this.props.tweet} className="twitter-share-button btn tweet" data-size="large" target="_blank"><i className="fab fa-twitter fa-lg" style={tweetStyle}></i></a>
-          <button onClick={this.props.loadQuote} className="btn quoteButton">Change Quote</button>
+          <a onClick={this.props.tweetQuote} href={this.props.tweet} className="twitter-share-button btn tweet" data-size="large" target="_blank" id="tweet-quote"><i className="fab fa-twitter fa-lg" style={tweetStyle}></i></a>
+          <button onClick={this.props.loadQuote} className="btn quoteButton" id="new-quote">Change Quote</button>
         </div>
       </div>
     );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 const defaultState = {
   quote: "Avoid popularity if you would have peace.",
   author: "Abraham Lincoln",
-  tweet: "",
+  tweet: "https://twitter.com/intent/tweet/",
 }
 
 const mainReducer = (state=defaultState, action) => {
@@ -9,7 +9,8 @@ const mainReducer = (state=defaultState, action) => {
     case "GET_QUOTE":
       return {...state, quote: action.quote, author: action.author};
     case "TWEET_QUOTE":
-      const hrefTweet = "https://twitter.com/intent/tweet/?text=" +
+      const hrefTweet = state.tweet +
+        "?text=" +
         state.quote +
         " - " +
         state.author;
